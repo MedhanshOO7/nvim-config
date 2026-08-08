f9d317fd55c901ba725fb0a6bf9bd4a738b167a8
MedhanshOO7 <Medhansh07@outlook.com>
2026-08-08 13:59:55 +0530

    feat(plugins): add quicker.nvim quickfix ui enhancer

---
+++ lua/plugins/quicker.lua
@@ -0,0 +1,10 @@
+return {
+    "stevearc/quicker.nvim",
+    event = "FileType qf",
+    opts = {
+        keys = {
+            { ">", function() require("quicker").expand({ before = 2, after = 2, add_to_existing = true }) end, desc = "Expand context" },
+            { "<", function() require("quicker").collapse() end, desc = "Collapse context" },
+        },
+    },
+}
