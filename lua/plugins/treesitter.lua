df169cebb2d9a377f6f965179677f604eab8007f
MedhanshOO7 <Medhansh07@outlook.com>
2026-08-08 13:59:48 +0530

    feat(treesitter): update treesitter grammar parsers and highlighting

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,88 +1,107 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    event = { "BufReadPost", "BufNewFile" },
-    dependencies = {
-        "windwp/nvim-ts-autotag",
-        "nvim-treesitter/nvim-treesitter-textobjects",
-    },
-    config = function()
-        require("nvim-treesitter").setup({
-            highlight = {
-                enable = true,
-                additional_vim_regex_highlighting = false,
-            },
-            indent = { enable = true },
-            incremental_selection = {
-                enable = true,
-                keymaps = {
-                    init_selection = "gnn",
-                    node_incremental = "grn",
-                    scope_incremental = "grc",
-                    node_decremental = "grm",
-                },
-            },
-            ensure_installed = {
-                "bash",
-                "c",
-                "cmake",
-                "comment",
-                "cpp",
-                "css",
-                "devicetree",
-                "diff",
-                "dockerfile",
-                "html",
-                "hyprlang",
-                "javascript",
-                "json",
-                "jsonc",
-                "latex",
-                "lua",
-                "luadoc",
-                "markdown",
-                "markdown_inline",
-                "mermaid",
-                "python",
-                "qml",
-                "query",
-                "regex",
-                "toml",
-                "tsx",
-                "typescript",
-                "vim",
-                "vimdoc",
-                "yaml",
-            },
-            auto_install = true,
-            textobjects = {
-                select = {
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        event = { "BufReadPost", "BufNewFile" },
+        dependencies = {
+            "windwp/nvim-ts-autotag",
+            "nvim-treesitter/nvim-treesitter-textobjects",
+        },
+        config = function()
+            require("nvim-treesitter").setup({
+                highlight = {
                     enable = true,
-                    lookahead = true,
+                    additional_vim_regex_highlighting = { "markdown" },
+                },
+                indent = { enable = true },
+                incremental_selection = {
+                    enable = true,
                     keymaps = {
-                        ["af"] = "@function.outer",
-                        ["if"] = "@function.inner",
-                        ["ac"] = "@class.outer",
-                        ["ic"] = "@class.inner",
-                        ["aa"] = "@parameter.outer",
-                        ["ia"] = "@parameter.inner",
+                        init_selection = "gnn",
+                        node_incremental = "grn",
+                        scope_incremental = "grc",
+                        node_decremental = "grm",
                     },
                 },
-                move = {
-                    enable = true,
-                    set_jumps = true,
-                    goto_next_start = {
-                        ["]m"] = "@function.outer",
-                        ["]]"] = "@class.outer",
+                ensure_installed = {
+                    "bash",
+                    "c",
+                    "cmake",
+                    "comment",
+                    "cpp",
+                    "css",
+                    "devicetree",
+                    "diff",
+                    "dockerfile",
+                    "go",
+                    "html",
+                    "hyprlang",
+                    "javascript",
+                    "json",
+                    "jsonc",
+                    "latex",
+                    "lua",
+                    "luadoc",
+                    "markdown",
+                    "markdown_inline",
+                    "mermaid",
+                    "python",
+                    "qml",
+                    "query",
+                    "regex",
+                    "ruby",
+                    "rust",
+                    "sql",
+                    "toml",
+                    "tsx",
+                    "typescript",
+                    "vim",
+                    "vimdoc",
+                    "yaml",
+                },
+                auto_install = true,
+                textobjects = {
+                    select = {
+                        enable = true,
+                        lookahead = true,
+                        keymaps = {
+                            ["af"] = "@function.outer",
+                            ["if"] = "@function.inner",
+                            ["ac"] = "@class.outer",
+                            ["ic"] = "@class.inner",
+                            ["aa"] = "@parameter.outer",
+                            ["ia"] = "@parameter.inner",
+                        },
                     },
-                    goto_previous_start = {
-                        ["[m"] = "@function.outer",
-                        ["[["] = "@class.outer",
+                    move = {
+                        enable = true,
+                        set_jumps = true,
+                        goto_next_start = {
+                            ["]m"] = "@function.outer",
+                            ["]]"] = "@class.outer",
+                        },
+                        goto_previous_start = {
+                            ["[m"] = "@function.outer",
+                            ["[["] = "@class.outer",
+                        },
                     },
                 },
-            },
-        })
+            })
 
-        require("nvim-ts-autotag").setup()
-    end,
+            require("nvim-ts-autotag").setup()
+        end,
+    },
+    {
+        "nvim-treesitter/nvim-treesitter-context",
+        event = { "BufReadPost", "BufNewFile" },
+        opts = {
+            max_lines = 3,
+            min_window_height = 0,
+            line_numbers = true,
+            multiline_threshold = 20,
+            trim_scope = "outer",
+            mode = "cursor",
+        },
+    },
 }
+
