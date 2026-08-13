f594345f0c4badce57ff51ec31cfc0d120055401
MedhanshOO7 <Medhansh07@outlook.com>
2026-08-13 20:02:49 +0530

    fix(live-server): resolve current file directory path to serve relative assets correctly

--- lua/plugins/live-server.lua
+++ lua/plugins/live-server.lua
@@ -1,10 +1,29 @@
 return {
     "barrett-ruth/live-server.nvim",
-    build = "npm i -g live-server || true",
-    cmd = { "LiveServerStart", "LiveServerStop" },
-    config = true,
+    cmd = { "LiveServerStart", "LiveServerStop", "LiveServerToggle" },
+    config = function()
+        require("live-server").setup({
+            args = {
+                "--port=5500",
+                "--host=127.0.0.1",
+            },
+        })
+    end,
     keys = {
-        { "<leader>lv", "<cmd>LiveServerStart<cr>", desc = "Live Server Start" },
+        {
+            "<leader>lv",
+            function()
+                local dir = vim.fn.expand("%:p:h")
+                local file = vim.fn.expand("%:t")
+                if dir ~= "" and dir ~= "." then
+                    vim.cmd("LiveServerStart " .. vim.fn.fnameescape(dir))
+                    vim.notify("Live Server started serving: " .. vim.fn.expand("%:t"), vim.log.levels.INFO, { title = "Live Server" })
+                else
+                    vim.cmd("LiveServerStart")
+                end
+            end,
+            desc = "Live Server Start (Current Directory)",
+        },
         { "<leader>lV", "<cmd>LiveServerStop<cr>", desc = "Live Server Stop" },
     },
 }
