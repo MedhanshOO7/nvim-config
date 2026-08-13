0d707dc8298c370840b528a0accf7c0d9fcfda2e
MedhanshOO7 <Medhansh07@outlook.com>
2026-08-13 20:04:04 +0530

    feat(markdown): add markdown-preview.nvim with auto-sync and persistent browser preview options

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -268,4 +268,23 @@ return {
             },
         },
     },
+    {
+        "iamcco/markdown-preview.nvim",
+        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+        ft = { "markdown" },
+        build = function() vim.fn["mkdp#util#install"]() end,
+        init = function()
+            vim.g.mkdp_auto_start = 0
+            vim.g.mkdp_auto_close = 0 -- Keep browser open & sync to current file when switching buffers!
+            vim.g.mkdp_refresh_slow = 0 -- Instant refresh on text change
+            vim.g.mkdp_command_for_global = 0
+            vim.g.mkdp_open_to_the_world = 0
+            vim.g.mkdp_echo_preview_url = 1
+            vim.g.mkdp_page_title = "「${name}」"
+        end,
+        keys = {
+            { "<leader>np", "<cmd>MarkdownPreviewToggle<cr>", desc = "Markdown Browser Preview (Auto-Sync)" },
+            { "<leader>mp", "<cmd>MarkdownPreviewToggle<cr>", desc = "Markdown Browser Preview (Auto-Sync)" },
+        },
+    },
 }
