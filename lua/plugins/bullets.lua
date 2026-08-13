0d3b4a10180cf2c0b6adb3c132f4e3c09f41b588
MedhanshOO7 <Medhansh07@outlook.com>
2026-08-13 18:59:43 +0530

    chore(plugins): disable redundant bullets.vim plugin

--- lua/plugins/bullets.lua
+++ lua/plugins/bullets.lua
@@ -1,14 +1,4 @@
 return {
     "dkarter/bullets.vim",
-    ft = { "markdown", "text", "gitcommit" },
-    init = function()
-        vim.g.bullets_enabled_file_types = {
-            "markdown",
-            "text",
-            "gitcommit",
-        }
-        -- Disable default mappings to avoid conflict with autolist.nvim
-        vim.g.bullets_set_mappings = 0
-        vim.g.bullets_mapping_leader = ""
-    end,
+    enabled = false,
 }
