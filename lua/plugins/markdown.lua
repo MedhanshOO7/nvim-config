cc0c0df696aa1bb04c9641a674669bf5977e7d29
MedhanshOO7 <Medhansh07@outlook.com>
2026-08-18 14:32:50 +0530

    fix(markdown): build markdown-preview node dependencies with npm install

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -271,8 +271,7 @@ return {
     {
         "iamcco/markdown-preview.nvim",
         cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-        ft = { "markdown" },
-        build = function() vim.fn["mkdp#util#install"]() end,
+        build = "cd app && npm install",
         init = function()
             vim.g.mkdp_auto_start = 0
             vim.g.mkdp_auto_close = 0 -- Keep browser open & sync to current file when switching buffers!
