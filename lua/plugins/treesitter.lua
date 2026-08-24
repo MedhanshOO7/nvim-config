70d9aad0a5d5e3cb9c7fb15aacd062c7976f27de
MedhanshOO7 <Medhansh07@outlook.com>
2026-08-25 04:06:58 +0530

    fix(ui): disable treesitter-context sticky header bar above code

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -98,16 +98,7 @@ return {
     },
     {
         "nvim-treesitter/nvim-treesitter-context",
-        event = { "BufReadPost", "BufNewFile" },
-        opts = {
-            max_lines = 3,
-            min_window_height = 0,
-            line_numbers = true,
-            multiline_threshold = 20,
-            trim_scope = "outer",
-            mode = "cursor",
-            separator = "─",
-        },
+        enabled = false,
     },
 }
 
