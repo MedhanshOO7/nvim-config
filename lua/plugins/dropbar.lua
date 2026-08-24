ca8a32934cedfe0516e53dfc3cb2dab60d192bf7
MedhanshOO7 <Medhansh07@outlook.com>
2026-08-25 04:04:01 +0530

    fix(breadcrumbs): deduplicate dropbar sources with fallback provider

--- lua/plugins/dropbar.lua
+++ lua/plugins/dropbar.lua
@@ -4,29 +4,62 @@ return {
     dependencies = {
         "nvim-tree/nvim-web-devicons",
     },
-    opts = {
-        bar = {
-            enable = function(buf, win)
-                return vim.fn.buflisted(buf) == 1
-                    and vim.api.nvim_buf_get_name(buf) ~= ""
-                    and not vim.tbl_contains({
-                        "help",
-                        "snacks_dashboard",
-                        "snacks_picker_input",
-                        "snacks_explorer",
-                        "alpha",
-                        "dashboard",
-                        "lazy",
-                        "mason",
-                        "trouble",
-                        "toggleterm",
-                        "notify",
-                        "noice",
-                    }, vim.bo[buf].filetype)
-            end,
-            separator = "  ",
-        },
-    },
+    opts = function()
+        local sources = require("dropbar.sources")
+        local utils = require("dropbar.utils")
+
+        return {
+            bar = {
+                enable = function(buf, _)
+                    return vim.fn.buflisted(buf) == 1
+                        and vim.api.nvim_buf_get_name(buf) ~= ""
+                        and not vim.tbl_contains({
+                            "help",
+                            "snacks_dashboard",
+                            "snacks_picker_input",
+                            "snacks_explorer",
+                            "alpha",
+                            "dashboard",
+                            "lazy",
+                            "mason",
+                            "trouble",
+                            "toggleterm",
+                            "notify",
+                            "noice",
+                        }, vim.bo[buf].filetype)
+                end,
+                separator = "  ",
+                sources = function(buf, _)
+                    if vim.bo[buf].ft == "markdown" then
+                        return {
+                            sources.path,
+                            sources.markdown,
+                        }
+                    end
+                    if vim.bo[buf].buftype == "terminal" then
+                        return {
+                            sources.terminal,
+                        }
+                    end
+                    return {
+                        sources.path,
+                        utils.source.fallback({
+                            sources.lsp,
+                            sources.treesitter,
+                        }),
+                    }
+                end,
+            },
+            sources = {
+                path = {
+                    relative_to = function(buf, _)
+                        local root = vim.fs.root(buf, { ".git", "package.json", "Makefile", "CMakeLists.txt" })
+                        return root or vim.fn.getcwd()
+                    end,
+                },
+            },
+        }
+    end,
     keys = {
         {
             "<leader>;",
